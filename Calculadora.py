558393c410039448e3eef6267b6c73b09d1d9972
Criss0211 <changeme@example.com>
2025-03-28 15:29:47 +0000

    First commit

---
+++ Calculadora.py
@@ -0,0 +1,5 @@
+print ('Bienvenido a mi calculadora')
+num1 = int(input("ingrese el primero numero: "))
+num2 = int(input("ingrese el segungo numero: "))
+
+print(f"La suma de los 2 valores es: {num1 + num2}")
